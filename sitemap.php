c4a4954a658bd7aba1449d3e63b40f128d6460eb
zainengineer <zainengineer@gmail.com>
2015-05-31 17:35:15 +0930

    added missing files

---
+++ sitemap.php
@@ -0,0 +1,51 @@
+<?php
+$PageTitleStart = "Site Map";
+include "start.php";
+
+?>
+
+<pre><b><font color="#0000CC"> Jobs</font></b>
+	<font color="#FF0000"><a href="recentjobs.htm">Recent Job</a>
+	<a href="cvsubmission.htm">Cv Submission</a>
+	<a href="directjobs.htm">Direct Jobs</a>
+	<a href="jobcalls.htm">Job Calls</a>
+	<a href="cvbrowsing.htm">Browse Cv</a></font>
+<b><font color="#0000CC">Software/Web Projects</font></b>
+	<a href="getdevelopmentproject.htm">Get Software And Web Projects</a>
+<font color="#0000CC"><b>Frontier Soft</b></font>
+	<font color="#FF0000"><a href="webdevelopment.html">Web Development</a>
+	<a href="webhosting.htm">Web Hosting</a>
+	<a href="software.htm">Software</a>
+	<a href="aboutus.htm">About Frontiersoft</a>
+	<a href="contactus.htm">Contact Us</a></font>		
+<b><font color="#0000CC">Immigration</font></b>
+	<font color="#FF0000"><a href="usa.htm">Education</a>
+	<a href="usa.htm">Jobs</a></font>		
+<b><font color="#0000CC">International Jobs</font></b>
+	<font color="#FF0000"><a href="general.htm">General</a>
+	<a href="middleeast.htm">Middle East</a>
+	<a href="europe.htm">Europe</a>
+	<a href="america.htm">America</a></font></pre>
+
+<noframes>
+	Projects <br/>
+	
+	<a href="http://www.connectpk.com">connectpk.com</a> <br/>
+	<a href="http://www.connectpk.com">ConnectPk.com, Mobile Terrifs, SMS, Call, MMS, Ring Tones</a> <br/>
+	<a href="http://www.connectpk.com">ConnectPk.com Warid Telenor ufone paknet mobilink jazz telenor</a> <br/>
+	
+	
+	
+	<a href="http://www.quickfranchising.com">QuickFranchising.com</a> <br/>
+	<a href="http://www.quickfranchising.com">Quick Franchising</a> <br/>
+	<a href="http://www.quickfranchising.com">Quick Franchise</a> <br/>
+</noframes>
+
+
+
+
+<?php
+
+include "end.php";
+
+?>
---
+++ webhosting.php
@@ -0,0 +1,285 @@
+<?php
+$PageTitleStart = "WebHosting + Domain Registration";
+include "start.php";
+
+?>
+<h2 align="center"><b><font size="+2" face="Arial, Helvetica, sans-serif">Economical 
+  Dot Com Package</font><font color="#FFFF00" size="+2" face="Arial, Helvetica, sans-serif"><br>
+  </font>
+  </b><font color="#000000" size="2" face="Arial, Helvetica, sans-serif"><em><b>Domain 
+  name (. com like yahoo.com or microsoft.com) Space on the internet with </b></em></font><font color="#000000" size="2" face="Arial, Helvetica, sans-serif"><em><b>many 
+  facilities.</b></em></font></h2>
+<div align="center"><strong>
+  <font color="#000099" size="+3" face="Courier New, Courier, mono">Features</font></strong></div>
+<table width="496" height="100" border="1" align="center" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#CCCCCC"> 
+    <td width="252"  height="130" bgcolor="#FFFFFF"> 
+<p><font color="#000099" size="2" face="Arial, Helvetica, sans-serif">
+	100  MB Web Space reserved.<br>
+        Unlimited E-mail accounts<br>
+        ( like chiefexecutive@yourcompany.com)</font><font size="1" face="Arial, Helvetica, sans-serif" color="#000099">.</font><font color="#000099"><br>
+		<font size="2" face="Arial, Helvetica, sans-serif">php 5 support.<br>
+		
+		<font size="2" face="Arial, Helvetica, sans-serif">Unlimited Mysql Databases<br>
+		
+		<font size="2" face="Arial, Helvetica, sans-serif">Built in phpmyadmin<br>
+		
+		<font size="2" face="Arial, Helvetica, sans-serif">Unlimited sub-domains<br>
+		
+		 <font size="2" face="Arial, Helvetica, sans-serif">Front page extension 
+        support .<br>
+        </font></font>
+		
+		<font size="2" face="Arial, Helvetica, sans-serif" color="#000099">CGI 
+        script.<br>
+        Perl 5 support .</font><br/>
+		
+		
+		</p>
+		
+    </td>
+    <td width="238" bgcolor="#FFFFFF" > 
+      <p><font size="2" face="Arial, Helvetica, sans-serif" color="#000099">Free 
+        Hits Counter. statistics packages<br>
+        Auto responders.<br>
+        Auto forwarders.<br>
+        24 hours FTP Access .<br>
+        
+        </font></p>
+    </td>
+  </tr>
+  <tr bgcolor="#ACDDFF"> 
+<td height="36" colspan="2"> 
+<div align="center"><font color="#000099" size="2"><b> Rs. </font>
+  <font color="#000099" size="+3">1300</font><font color="#000099" size="2">/yr</font></b></div>
+    </td>
+  </tr>
+</table>
+<p align="center"><strong><font size="+2">DETAILS</font></strong></p>
+<p align="center"><font face="Courier New, Courier, mono" size="+1"><strong><font size="+3">Free 
+  Services</font></strong></font></p>
+<table width="473" border="1" align="center" height="80" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#CCCCCC"> 
+    <td width="232" height="117" bgcolor="#FFFFFF"> 
+<p><font color="#000099" size="2" face="Arial, Helvetica, sans-serif"> Front 
+        page extension support<br>
+        CGI script<br>
+        Perl 5 support<br>
+        FTP upload facility<br>
+        Free Hits Counter.</font></p>
+    </td>
+    <td width="235" height="117" bgcolor="#FFFFFF" > <font color="#000099" size="2" face="Arial, Helvetica, sans-serif"> 
+      Free Hits Counter.<br>
+      Auto responders<br>
+      Auto forwarders<br>
+      24 hours FTP Access</font></td>
+  </tr>
+  <tr bgcolor="#000000"> 
+<td colspan="2" bgcolor="#ACDDFF"> 
+<div align="center"><b><font size="6" color="#000099">FREE</font></b></div>
+    </td>
+  </tr>
+</table>
+<p align="center"><font size="+1"><strong>
+<font face="Courier New, Courier, mono" size="+3">Domain 
+  Name</font></strong></font></p>
+<div align="center"> 
+  <table width="400" border="1" align="center" cellpadding="0" cellspacing="0">
+    <tr> 
+      <td width="347" bgcolor="#FFFFFF"> 
+<p align="center">
+<font face="Arial, Helvetica, sans-serif" size="2" color="#000099">.com, 
+          .org, .net <font size="5"><b>Rs.700</b><font size="2"> /yr</font></font></font></p>
+        <p align="center">
+        <font color="#000099" size="2" face="Arial, Helvetica, sans-serif">pknic.com.pk, 
+          .net.pk, .org.pk, Cost: Rs.2200 for two years.</font></p>
+      </td>
+    </tr>
+  </table>
+</div>
+<p align="center"> <strong><font size="+3" face="Courier New, Courier, mono">Unix 
+  Hosting </font></strong></p>
+<div align="center">
+  <center>
+<table width="470" border="1" height="100" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111">
+  <tr bgcolor="#000000"> 
+    <td bgcolor="#ACDDFF"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><strong>Unix Silver</strong></font> </div></td>
+    <td bgcolor="#ACDDFF"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><strong>Unix Gold</strong> </font> </div></td>
+    <td bgcolor="#ACDDFF"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><strong>Unix Platinum</strong> </font> </div></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td width="167" height="50" bgcolor="#FFFFFF" valign="top"> <div align="left"> 
+        <p>
+          <font size="2" face="Arial, Helvetica, sans-serif" color="#000099">
+        <br>
+        <br>
+        100 MB Web Space <br>
+				1000 MB Bandwidth <br>
+        </font></p>
+      </div></td>
+    <td width="170" height="96" bgcolor="#FFFFFF" valign="top"> <div align="left"> 
+        <p><font color="#000099" size="2" face="Arial, Helvetica, sans-serif">
+        <br>
+        <br>
+        200 MB Web Space <br>
+				2000 MB Bandwidth
+        </p>
+      </div></td>
+    <td width="170" height="96" bgcolor="#FFFFFF" valign="top"> <div align="left"> 
+        <p><font color="#000099" size="2" face="Arial, Helvetica, sans-serif">
+        <br>
+        <br>
+        500 MB Web Space <br>
+				5000 MB Bandwidth
+         </font></p>
+      </div></td>
+  </tr>
+  <tr bgcolor="#000000"> 
+    <td width="167" height="25" bgcolor="#ACDDFF"> <div align="center"><b>
+      <font size="5" color="#000099">Rs. 1300<font size="2">/Yr</font></font></b></div></td>
+    <td width="170" height="25" bgcolor="#ACDDFF"> <div align="center"><b>
+      <font size="5" color="#000099">Rs. 1700<font size="2">/Yr</font></font></b></div></td>
+    <td width="170" height="25" bgcolor="#ACDDFF"> <div align="center"><b>
+      <font size="5" color="#000099">Rs. 2500<font size="2">/Yr</font></font></b></div></td>
+  </tr>
+</table>
+  </center>
+</div>
+<p align="center">&nbsp;</p>
+<p align="center"><strong><font size="+3" face="Courier New, Courier, mono">Windows 
+  Hosting </font></strong></p>
+<table width="505" height="100" border="1" align="center" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#000000"> 
+    <td bgcolor="#ACDDFF" width="152"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><b>ASP Silver</b></font> </div></td>
+    <td bgcolor="#ACDDFF" width="153"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><strong>ASP Gold</strong></font></div></td>
+    <td bgcolor="#ACDDFF" width="192"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><strong>ASP Plus</strong></font></div></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td width="152" height="147" bgcolor="#FFFFFF" valign="top">  
+        <p><font face="Arial, Helvetica, sans-serif" size="2" color="#000099">
+        <br>
+        50 MB Web Space <br>
+        3 POP3 Email Accounts<br>
+          Unlimited Forwards<br>
+        Access Database<br>
+        mySQL Support</font></p>
+      </td>
+    <td width="153" height="147" bgcolor="#FFFFFF" valign="top"> <font color="#000099" size="2" face="Arial, Helvetica, sans-serif">
+    <br>
+        100 MB Web Space<br>
+    10 POP3 Accounts<br>
+    Unlimited Forwards<br>
+    Access Database<br>
+    mySQL Support<br>
+    ASP.NET Support</font></td>
+    <td width="192" height="147" bgcolor="#FFFFFF" valign="top"> <div align="justify"><font color="#000099" size="2" face="Arial, Helvetica, sans-serif"><br>
+        100 MB Web Space reserved<br>
+        Unlimited POP3 Email Accounts<br>
+      Unlimited Forwards<br>
+        Access Support<br>
+      mySQL Support<br>
+      ASP.NET Support<br>
+      SQL-Server Support<br>
+&nbsp;</font></div></td>
+  </tr>
+  <tr bgcolor="#000000"> 
+    <td width="152" bgcolor="#ACDDFF"> <div align="center">
+      <font color="#000099"><b><font size="5">Rs. 1800<font size="2">/Yr</font></font></b></font></div></td>
+    <td width="153" bgcolor="#ACDDFF"> <div align="center">
+      <font color="#000099"><b><font size="5">Rs. 3200<font size="2">/Yr</font></font></b></font></div></td>
+    <td width="192" bgcolor="#ACDDFF"> <div align="center">
+      <font color="#000099"><b><font size="5">Rs. 4000<font size="2">/Yr</font></font></b></font></div></td>
+  </tr>
+</table>
+<p align="center"><strong><font size="+3" face="Courier New, Courier, mono">Giga 
+Hosting </font></strong></p>
+<table width="505" height="100" border="1" align="center" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#000000"> 
+    <td bgcolor="#ACDDFF" width="260"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><b>Unix Giga</b></font> </div></td>
+    <td bgcolor="#ACDDFF" width="239"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><strong>ASP/Windows Giga</strong></font></div></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td width="260" height="147" bgcolor="#FFFFFF" valign="top">  
+        <p><font face="Arial, Helvetica, sans-serif" size="2" color="#000099">
+        <br>
+        </font>
+        <font face="Arial, Helvetica, sans-serif" color="#000099" size="4"><b>1 
+        GB </b>Web Space <br>
+        + </font>
+        <font face="Arial, Helvetica, sans-serif" color="#000099" size="2">All 
+        Features of Unix Platinum + More</font></p>
+      </td>
+    <td width="239" bgcolor="#FFFFFF" valign="top"> <font color="#000099" size="2" face="Arial, Helvetica, sans-serif">
+    <br>
+    </font><font face="Arial, Helvetica, sans-serif" color="#000099" size="4">
+    <b>1 GB </b>Web Space </font><font color="#000099" size="2" face="Arial, Helvetica, sans-serif">
+    <br>
+    </font><b>
+    <font color="#000099" size="4" face="Arial, Helvetica, sans-serif">+</font><font color="#000099" size="2" face="Arial, Helvetica, sans-serif"> </font>
+    </b><font color="#000099" size="2" face="Arial, Helvetica, sans-serif">All 
+    Features of ASP Plus+ More</font></td>
+  </tr>
+  <tr bgcolor="#000000"> 
+    <td width="260" bgcolor="#ACDDFF"> <div align="center">
+      <font color="#000099"><b><font size="5">Rs.7000<font size="2">/Yr</font></font></b></font></div></td>
+    <td width="239" bgcolor="#ACDDFF"> <div align="center">
+      <font color="#000099"><b><font size="5">Rs. 8500<font size="2">/Yr</font></font></b></font></div></td>
+  </tr>
+</table>
+<p align="center"><strong><font face="Courier New, Courier, mono" size="+3">
+Dedicated Hosting </font></strong></p>
+<table width="505" height="100" border="1" align="center" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#000000"> 
+    <td bgcolor="#ACDDFF"> <div align="center">
+      <font size="+1" face="Courier New, Courier, mono"><b>Super Celeron Hosting</b></font></div></td>
+  </tr>
+  <tr bgcolor="#CCCCCC"> 
+    <td bgcolor="#FFFFFF" valign="top">  
+        <p><font face="Arial, Helvetica, sans-serif" size="2" color="#000099">
+        <br>
+        *Indicates standard configuration<br>
+        </font><font face="Arial, Helvetica, sans-serif" color="#000099"><b>
+        Unlimited Domains</b></font><font face="Arial, Helvetica, sans-serif" size="2" color="#000099"><br>
+        Processor Intel 2.4GHz Celeron Processor <br>
+        RAM 1GB RAM*, Upgradeable to 2GB <br>
+        </font><font face="Arial, Helvetica, sans-serif" color="#000099"><b>Hard 
+        Drive 80GB</b></font><font face="Arial, Helvetica, sans-serif" size="2" color="#000099"> 
+        HDD*, Upgradeable to 2 x 200GB HDDs <br>
+        </font><font face="Arial, Helvetica, sans-serif" color="#000099"><b>
+        Bandwidth 1200GB</b></font><font face="Arial, Helvetica, sans-serif" size="2" color="#000099"> 
+        Bandwidth*, Upgradeable to 1500GB <br>
+        Uplink Port 10Mbps*, Upgradeable to 100Mbps <br>
+        IP Addresses 5 IP Addresses*, Upgradeable to 61 IP Addresses <br>
+        Operating System Choice of RedHat Enterprise Linux 3.0*, RedHat 
+        Enterprise Linux 4.0, Microsoft Windows 2000 Server, Microsoft Windows 
+        Server 2003, Debian, or FreeBSD <br>
+        Control Panel Available cPanel, Helm, or Plesk <br>
+        Web Analytics Available Urchin 5.0 <br>
+        Database Available MySQL, Shared or Dedicated Microsoft SQL Server 2000, 
+        Oracle 10G Standard Edition One <br>
+        Firewall Available Cisco Pix 506e or 515e <br>
+        Professional Services Plan Silver*, Gold, Platinum or Titanium</font></p>
+      </td>
+  </tr>
+  <tr bgcolor="#000000"> 
+    <td width="152" bgcolor="#ACDDFF"> <div align="center">
+      <font size="5" color="#000099"><b>$220</b></font><font color="#000099"><b><font size="2">/</font></b></font><font size="2" color="#000099"><b>mo</b></font></div></td>
+  </tr>
+</table>
+<p align="center">&nbsp;</p>
+<p align="center"><font color="#000099"><b>For More dedicated hosting plans or 
+other shared hosting plans call <font size="4">0321-9053094</font></b></font></p>
+
+
+<?php
+
+include "end.php";
+?>
